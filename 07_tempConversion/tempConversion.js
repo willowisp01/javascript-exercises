38c74e4deb57251b66643f365855248ab1f624e5
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2025-01-04 12:58:34 +0800

    Finish tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+  // F to C 
+  return parseFloat(((degF - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  // C to F 
+  return parseFloat((degC * 9 / 5 + 32).toFixed(1)); 
 };
 
 // Do not edit below this line
