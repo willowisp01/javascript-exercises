0f027c651f6a16b018a9753a291c1e589aab5660
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2025-01-04 12:40:54 +0800

    Finish sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    if (!Number.isInteger(start) || !Number.isInteger(end) || start < 0 || end < 0) {
+        return 'ERROR';
+    }
+    if (start > end) {
+        temp = start;
+        start = end; 
+        end = temp;
+    }
+    let answer = 0;
+    while (start <= end) {
+        answer += start;
+        start++;
+    }
+    return answer;
 };
 
 // Do not edit below this line
