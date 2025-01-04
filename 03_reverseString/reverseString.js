ad16ec9e7cbbc5bb6f28a6c726cc824f9bbb3c56
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2025-01-04 12:02:57 +0800

    Finish reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const charArray = string.split(""); 
+    let answer = ""; 
+    for (let i = charArray.length - 1; i >= 0; i--) {
+        answer += charArray[i];
+    }
+    return answer;
 };
 
 // Do not edit below this line
