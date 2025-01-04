e32c61817aa5bd7a83550c7fd8fcf38ed9efb70f
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2025-01-04 11:52:33 +0800

    Finish repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repetitions) {
+    result = ""; 
+    if (repetitions < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < repetitions; i++) {
+        result += string;
+    }
+    return result;
 };
 
 // Do not edit below this line
