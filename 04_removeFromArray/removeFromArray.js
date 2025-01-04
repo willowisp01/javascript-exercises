e69a63db75920fb2b7900e54eb872a657a6d02e1
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2025-01-04 12:28:28 +0800

    Finish removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const myRemoveFromArray = function(original, ...toBeRemoved) {
+    for (let item of toBeRemoved) { 
+        // while lets us remove multiple items.
+        while (original.indexOf(item) >= 0) { // if item to be removed exists in the original
+            //remove that item (in-place)
+            original.splice(original.indexOf(item), 1);
+        }
+    }
+    return original;
+};
+
+const removeFromArray = function(original, ...toBeRemoved) {
+    // using filter. Include items not in toBeRemoved.
+    return original.filter((item) => !toBeRemoved.includes(item));
 };
 
 // Do not edit below this line
