8b8c73c01b0b338226262a4712a5fbe48f480199
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2025-01-09 14:10:31 +0800

    Finish findTheOldest
    All exercises completed!!!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let currentYear = new Date().getFullYear();
+    // assuming array not empty
+    return people.reduce((oldestSoFar, current) => 
+        getAge(current, currentYear) > getAge(oldestSoFar, currentYear) ? current : oldestSoFar
+    );
 };
+
+function getAge(person, currentYear) {
+    if (!("yearOfDeath" in person)) {
+        return currentYear - person.yearOfBirth; 
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
