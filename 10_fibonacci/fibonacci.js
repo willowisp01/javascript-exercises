76e4fc59efef30e07a38a0f69278fc7ac7f3855f
willowisp01 <132592621+willowisp01@users.noreply.github.com>
2025-01-09 13:50:57 +0800

    Finish fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    input = +input;
+    if (Number.isNaN(input)) {
+        return "OOPS";
+    }
+    if (input < 0) {
+        return "OOPS"; 
+    }
+    if (input <= 1) {
+        return input;
+    }
+    let prev = 0;
+    let curr = 1;
+    for (let i = 2; i <= input; i++) {
+        let temp = prev + curr; 
+        prev = curr;
+        curr = temp;
+    }
+    return curr;
 };
 
 // Do not edit below this line
